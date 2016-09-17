955142b5058c88cb8e34b144c9691700e9959923
Michael <michaelhuytran@gmail.com>
2016-09-17 13:54:01 -0700

    [feature] - added reddis into node

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import Landing from './components/landing.js';
+import LandingApp from './component/landingApp.js';
 
 
 
 ReactDom.render((
 	<Router history={browserHistory}>
-		<Route path="/" component={Landing}/>
+		<Route path="/" component={LandingApp}/>
 	</Router>
 	), document.getElementById("app"));
-
-
-
-
--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const redis = require("redis"),
+
 const app = express();
 const path = require('path');
+const client = redis.createClient();
+
 
 app.use(express.static('./client'));
 
@@ -9,5 +13,5 @@ app.get('*', function (request, response){
 });
 
 app.set('port', process.env.PORT || 3000);
-app.listen(app.get('port'), () => console.log('we are listening on: ', app.get('port')));
+app.listen(app.get('port'), () => console.log('We are listening on: ', app.get('port')));
 
