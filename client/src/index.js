3d657f483ff86a806ba8867736ef15ee40476a82
Michael <michaelhuytran@gmail.com>
2016-09-17 13:54:01 -0700

    [feature] - added reddis into node

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { Router, browserHistory } from 'react-router';
-
+import LandingApp from './component/landingApp.js';
 import routes from './routes';
 import Landing from './components/Landing';
 import  reducers  from './reducers';
@@ -12,5 +12,6 @@ ReactDOM.render(
   <Provider store={createStore(reducers)}>
     <Landing />
     <Router history={browserHistory} routes={routes} />
+    <Route path="/" component={LandingApp}/>
   </Provider>
   , document.querySelector('.container'));
