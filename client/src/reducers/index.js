4c0c673cc12e3467b0de8ec1ad0e12b1b6ae344e
= <Jmitchell1991@gmail.com>
2016-10-04 20:17:28 -0700

    [deploy] - modifies file for deployment

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,7 +3,7 @@ import quizReducer from './reducer_quiz';
 import userProfileReducer from './reducer_userProfile';
 import userInputReducer from './reducer_userInput';
 import coordinatesReducer from './reducer_coordinates';
-import receivedCitiesReducer from './reducer_receivedCities';
+import receivedCitiesReducer from './reducer_receiveCities';
 
 const rootReducer = combineReducers({
   quiz: quizReducer,
